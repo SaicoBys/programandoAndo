a330048b542efba9734494ebd5d661916548f99f
Jacob Moronta Martínez <jacobmoronta0000@gmail.com>
2022-10-15 01:45:19 -0400

    Functions update

--- programacionBasica/fundamentos/mokepon/scripts/mokepon.js
+++ programacionBasica/fundamentos/mokepon/scripts/mokepon.js
@@ -1,22 +1,22 @@
 function iniciarJuego() {
-    let botonMascotaJugador = document.getElementById("boton-mascota");
-    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
 function seleccionarMascotaJugador() {
-    let inputHipodoge = document.getElementById('Hipodoge')
-    let inputCapipepo = document.getElementById('Capipepo')
-    let inputRatigueya = document.getElementById('Ratigueya')
-
-    if (inputHipodoge.checked){
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    
+    if (inputHipodoge.checked) {
         alert('Seleccionaste a Hipodoge')
-    }
-else if (inputCapipepo.checked) {
+    } else if (inputCapipepo.checked) {
         alert('Seleccionaste a Capipepo')
-    }else if (inputRatigueya.checked) {
+    } else if (inputRatigueya.checked) {
         alert('Seleccionaste a Ratigueya')
+    } else {
+        alert('Selecciona una mascota')
     }
-
 }
 
-window.addEventListener("load", iniciarJuego)
+window.addEventListener('load', iniciarJuego)
