99f50ae0cef1fc9c000e9f0587d510e7ab217fc5
Jacob Moronta Martínez <jacobmoronta0000@gmail.com>
2022-10-19 01:47:40 -0400

    Muchos cambios nuevos

--- programacionBasica/fundamentos/mokepon/scripts/mokepon.js
+++ programacionBasica/fundamentos/mokepon/scripts/mokepon.js
@@ -1,22 +1,44 @@
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
+    /* Creamos una variable para guardar el boton-mascota de HTML, en JS y poder manipularlo */
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+    /* Aqui usamos la variable que creamos para manipular el boton-mascota, le agregamos un "escucha, click, que va a disparar la funcion seleccionarMascotaJugador */
 }
 
 function seleccionarMascotaJugador() {
+    /* Aqui creamos las variables donde vamos a guardar los mokepon, usando un Document.getElementById, para guarda cada mokepon en una variable y poder usarla mas adelante */
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
     
+/* Aqui usamos las varaibles donde guardamos los mokepon, para hacer la logica del programa */
     if (inputHipodoge.checked) {
-        alert('Seleccionaste a Hipodoge')
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (inputCapipepo.checked) {
-        alert('Seleccionaste a Capipepo')
+        spanMascotaJugador.innerHTML = 'Capipepo'
     } else if (inputRatigueya.checked) {
-        alert('Seleccionaste a Ratigueya')
-    } else {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } /* Si no se selecciona nada, esete mensaje se va a mostrar */else {
         alert('Selecciona una mascota')
     }
 }
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
 window.addEventListener('load', iniciarJuego)
