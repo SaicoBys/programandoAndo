278547422a1abb31e874a6e5a11d11ad457e5102
Jacob Moronta Martínez <jacobmoronta0000@gmail.com>
2022-10-19 01:49:16 -0400

    Error

--- programacionBasica/fundamentos/mokepon/scripts/mokepon.js
+++ programacionBasica/fundamentos/mokepon/scripts/mokepon.js
@@ -1,27 +1,25 @@
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
-    /* Creamos una variable para guardar el boton-mascota de HTML, en JS y poder manipularlo */
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
-    /* Aqui usamos la variable que creamos para manipular el boton-mascota, le agregamos un "escucha, click, que va a disparar la funcion seleccionarMascotaJugador */
 }
 
 function seleccionarMascotaJugador() {
-    /* Aqui creamos las variables donde vamos a guardar los mokepon, usando un Document.getElementById, para guarda cada mokepon en una variable y poder usarla mas adelante */
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
     let spanMascotaJugador = document.getElementById('mascota-jugador')
     
-/* Aqui usamos las varaibles donde guardamos los mokepon, para hacer la logica del programa */
     if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (inputCapipepo.checked) {
         spanMascotaJugador.innerHTML = 'Capipepo'
     } else if (inputRatigueya.checked) {
         spanMascotaJugador.innerHTML = 'Ratigueya'
-    } /* Si no se selecciona nada, esete mensaje se va a mostrar */else {
+    } else {
         alert('Selecciona una mascota')
     }
+
+    seleccionarMascotaEnemigo()
 }
 
 function seleccionarMascotaEnemigo() {
@@ -38,7 +36,7 @@ function seleccionarMascotaEnemigo() {
 }
 
 function aleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 window.addEventListener('load', iniciarJuego)
