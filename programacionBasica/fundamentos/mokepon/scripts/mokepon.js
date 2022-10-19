9ffa6bf2e1457acb4b960f6c2d7948c8982f28c5
Jacob Moronta Martínez <jacobmoronta0000@gmail.com>
2022-10-19 01:53:35 -0400

    Error

--- programacionBasica/fundamentos/mokepon/scripts/mokepon.js
+++ programacionBasica/fundamentos/mokepon/scripts/mokepon.js
@@ -1,21 +1,25 @@
 function iniciarJuego() {
+    
     let botonMascotaJugador = document.getElementById('boton-mascota')
+     /* Creamos una variable para guardar el boton-mascota de HTML, en JS y poder manipularlo */
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
 function seleccionarMascotaJugador() {
+    /* Aqui creamos las variables donde vamos a guardar los mokepon, usando un Document.getElementById, para guarda cada mokepon en una variable y poder usarla mas adelante */
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
     let spanMascotaJugador = document.getElementById('mascota-jugador')
     
+    /* Aqui usamos las varaibles donde guardamos los mokepon, para hacer la logica del programa */
     if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (inputCapipepo.checked) {
         spanMascotaJugador.innerHTML = 'Capipepo'
     } else if (inputRatigueya.checked) {
         spanMascotaJugador.innerHTML = 'Ratigueya'
-    } else {
+    } /* Si no se selecciona nada, esete mensaje se va a mostrar */ else {
         alert('Selecciona una mascota')
     }
 
